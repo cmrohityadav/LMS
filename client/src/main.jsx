bcb5ceabcf40124d1a3a9f1f319f2d395773acf9
cmrohityadav <cmrohityadav23@gmail.com>
2024-04-07 12:43:54 +0530

    add redux store and auth slice

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,17 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import {Toaster} from 'react-hot-toast'
+import {Provider} from 'react-redux'
+import store from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
     <Toaster />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
 
